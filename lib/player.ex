a175b52d1cb61f84324a7e798a744bbc0dcbece8
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-01-11 21:51:13 -0300

    feat: add mandatory parameters for the player

--- lib/player.ex
+++ lib/player.ex
@@ -1,3 +1,4 @@
 defmodule ExMon.Player do
+  @enforce_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
   defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -2,7 +2,13 @@ defmodule PlayerTest do
   use ExUnit.Case
 
   test "should be able to create a player" do
-    player = %ExMon.Player{life: 100, name: "Player 1", move_rnd: :punch, move_avg: :kick, move_heal: :cure}
+    player = %ExMon.Player{
+      life: 100,
+      name: "Player 1",
+      move_rnd: :punch,
+      move_avg: :kick,
+      move_heal: :cure
+    }
 
     assert player.life == 100
     assert player.name == "Player 1"
@@ -10,4 +16,21 @@ defmodule PlayerTest do
     assert player.move_avg == :kick
     assert player.move_heal == :cure
   end
+
+  @required_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+  Enum.each(@required_keys, fn key ->
+    test "should raise exception when #{inspect(key)} key is not passed" do
+      incomplete_attrs =
+        Map.delete(
+          %{life: 100, name: "Player 1", move_rnd: :punch, move_avg: :kick, move_heal: :cure},
+          unquote(key)
+        )
+
+      assert_raise ArgumentError,
+                   ~r/the following keys must also be given when building struct ExMon.Player/,
+                   fn ->
+                     struct!(ExMon.Player, incomplete_attrs)
+                   end
+    end
+  end)
 end
